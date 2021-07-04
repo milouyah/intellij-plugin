5523a9dff611218a58832c9d5b3738675cd78d5a
GitHub Action <action@github.com>
2021-07-04 07:21:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/milouyah/intellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/milouyah/intellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.milouyah.intellijplugin.services
 
+import com.github.milouyah.intellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
